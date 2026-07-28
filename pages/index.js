639640fed9008668b4c510664aa1fb70d119975d
SK4339 <shouryakumar9559@gmail.com>
2026-07-29 00:56:56 +0530

    chore: add pages/index to satisfy Next.js build

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', padding: 24 }}>
+      <h1>AuraClinic</h1>
+      <p>Temporary index page to satisfy Next.js build.</p>
+    </div>
+  );
+}
